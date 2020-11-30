221c0db9b2d57b83e2b276aaa3a787e6d29f4d03
Momade Abdul <momademha@gmail.com>
2020-11-30 14:46:22 +0200

    Template basico da pagina home

---
+++ src/main/java/mz/co/devtec/loja/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package mz.co.devtec.loja.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home () {
+		return "home";
+	}
+
+}
