33c8a95466b3112e660c2d0c40f69896f75b4183
Momade Abdul <momademha@gmail.com>
2020-11-30 17:29:15 +0200

    Mostrando paginas na tabela - tela de admin

--- src/main/java/mz/co/devtec/loja/controllers/PaginaAdminController.java
+++ src/main/java/mz/co/devtec/loja/controllers/PaginaAdminController.java
@@ -1,15 +1,35 @@
 package mz.co.devtec.loja.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import mz.co.devtec.loja.models.data.Pagina;
+import mz.co.devtec.loja.repository.PaginaRepository;
 
 @Controller
 @RequestMapping("/admin/paginas")
 public class PaginaAdminController {
 	
+	@Autowired
+	private PaginaRepository paginaRepository;
+	
+	public PaginaAdminController(PaginaRepository paginaRepository) {
+		this.paginaRepository = paginaRepository;
+	} 
+	
+	
 	@GetMapping
-	public String index() {
+	public String index(Model model) {
+		
+		List<Pagina> paginas = paginaRepository.findAll();
+		
+		model.addAttribute("paginas",paginas);
+		
 		return "admin/paginas/index";
 	}
 
---
+++ src/main/java/mz/co/devtec/loja/repository/PaginaRepository.java
@@ -0,0 +1,9 @@
+package mz.co.devtec.loja.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import mz.co.devtec.loja.models.data.Pagina;
+
+public interface PaginaRepository extends JpaRepository<Pagina, Integer> {
+
+}
