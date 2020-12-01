8342046fce3ed2f1003ec35e0e5d8ef67d4dbf34
Momade Abdul <momademha@gmail.com>
2020-12-01 22:08:11 +0200

    Metodo e pagina de actualizacao de Paginas adicionado e funcionando. A pagina de actualizar oculta o slug caso o mesmo tenha o valor 'home'

--- src/main/java/mz/co/devtec/loja/controllers/PaginaAdminController.java
+++ src/main/java/mz/co/devtec/loja/controllers/PaginaAdminController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -70,6 +71,50 @@ public class PaginaAdminController {
 		}
 		
 		return "redirect:/admin/paginas/adicionar";
+	}
+	
+	
+	@GetMapping("/actualizar/{id}")
+	public String actualizar(@PathVariable int id, Model model) {
+		
+		Pagina pagina = paginaRepository.getOne(id);
+		
+		model.addAttribute("pagina", pagina);
+		
+		return "admin/paginas/actualiza";
+	}
+	
+	@PostMapping("/actualizar")
+	public String actualizar(@Valid Pagina pagina, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+		
+		Pagina paginaActual = paginaRepository.getOne(pagina.getId());
+		
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("tituloDaPagina", paginaActual.getTitulo());
+			
+			return "admin/paginas/actualiza";			
+		}
+		
+		redirectAttributes.addFlashAttribute("message", "Pagina actualizada com Sucesso");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		
+		String slug = pagina.getSlug() == "" ? pagina.getTitulo().toLowerCase().replace(" ", "-") : pagina.getSlug().toLowerCase().replace(" ", "-");
+		
+		Pagina oSlugExiste = paginaRepository.findBySlug(pagina.getId(),slug);
+		
+		if (oSlugExiste != null) {
+			
+			redirectAttributes.addFlashAttribute("message", "O slug existe, escolha outro");
+			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+			redirectAttributes.addFlashAttribute("pagina", pagina);
+		}
+		else {
+			pagina.setSlug(slug);
+		
+			paginaRepository.save(pagina);
+		}
+		
+		return "redirect:/admin/paginas/actualizar/" + pagina.getId();
 	}
 	
 	
--- src/main/java/mz/co/devtec/loja/repository/PaginaRepository.java
+++ src/main/java/mz/co/devtec/loja/repository/PaginaRepository.java
@@ -1,11 +1,15 @@
 package mz.co.devtec.loja.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import mz.co.devtec.loja.models.data.Pagina;
 
 public interface PaginaRepository extends JpaRepository<Pagina, Integer> {
 	
 	Pagina findBySlug(String slug);
+	
+	@Query("select p from Pagina p where p.id != :id and p.slug = :slug")
+	Pagina findBySlug(int id, String slug);
 
 }
