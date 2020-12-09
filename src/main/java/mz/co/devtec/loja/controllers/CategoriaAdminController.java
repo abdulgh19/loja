9e18d6d2aae655a571ae842dcd0b71894449c777
Momade Abdul <momademha@gmail.com>
2020-12-09 18:06:21 +0200

    Actualizacao de categoria tambem funcionando em pleno

--- src/main/java/mz/co/devtec/loja/controllers/CategoriaAdminController.java
+++ src/main/java/mz/co/devtec/loja/controllers/CategoriaAdminController.java
@@ -22,129 +22,124 @@ import mz.co.devtec.loja.repository.CategoriaRepository;
 @Controller
 @RequestMapping("/admin/categorias")
 public class CategoriaAdminController {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping
 	public String index(Model model) {
-		
+
 		List<Categoria> categorias = categoriaRepository.findAllByOrderBySortingAsc();
-		
-		model.addAttribute("categorias",categorias);
-		
+
+		model.addAttribute("categorias", categorias);
+
 		return "admin/categorias/index";
 	}
-	
+
 	@GetMapping("/adicionar")
 	public String adicionar(Categoria categoria) {
-		
+
 		return "admin/categorias/adiciona";
 	}
-	
-		
+
 	@PostMapping("/adicionar")
-	public String adicionar(@Valid Categoria categoria, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-		
+	public String adicionar(@Valid Categoria categoria, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes, Model model) {
+
 		if (bindingResult.hasErrors()) {
-			
-			return "admin/categorias/adiciona";			
+
+			return "admin/categorias/adiciona";
 		}
-		
+
 		redirectAttributes.addFlashAttribute("message", "Categoria adicionada com Sucesso");
 		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
-		
+
 		String slug = categoria.getNome().toLowerCase().replace(" ", "-");
-		
+
 		Categoria categoriaExiste = categoriaRepository.findByNome(categoria.getNome());
-		
+
 		if (categoriaExiste != null) {
-			
+
 			redirectAttributes.addFlashAttribute("message", "A Categoria existe, escolha outra");
 			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
 			redirectAttributes.addFlashAttribute("categoriaInfo", categoria);
-		}
-		else {
+		} else {
 			categoria.setSlug(slug);
 			categoria.setSorting(100);
-			
+
 			categoriaRepository.save(categoria);
 		}
-		
+
 		return "redirect:/admin/categorias/adicionar";
 	}
-	
-	
+
 	@GetMapping("/actualizar/{id}")
 	public String actualizar(@PathVariable int id, Model model) {
-		
+
 		Categoria categoria = categoriaRepository.getOne(id);
-		
+
 		model.addAttribute("categoria", categoria);
-		
+
 		return "admin/categorias/actualiza";
 	}
-	
+
 	@PostMapping("/actualizar")
-	public String actualizar(@Valid Categoria categoria, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-		
+	public String actualizar(@Valid Categoria categoria, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes, Model model) {
+
 		Categoria categoriaActual = categoriaRepository.getOne(categoria.getId());
-		
+
 		if (bindingResult.hasErrors()) {
-			model.addAttribute("tituloDaCategoria", categoriaActual.getNome());
-			
-			return "admin/categorias/actualiza";			
+			model.addAttribute("nomeDaCategoria", categoriaActual.getNome());
+
+			return "admin/categorias/actualiza";
 		}
-		
+
 		redirectAttributes.addFlashAttribute("message", "Categoria actualizada com Sucesso");
 		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
-		
-		String slug = categoria.getSlug() == "" ? categoria.getNome().toLowerCase().replace(" ", "-") : categoria.getSlug().toLowerCase().replace(" ", "-");
-		
-		Categoria oSlugExiste = categoriaRepository.findBySlug(categoria.getId(),slug);
-		
-		if (oSlugExiste != null) {
-			
-			redirectAttributes.addFlashAttribute("message", "O slug existe, escolha outro");
+
+		String slug = categoria.getNome().toLowerCase().replace(" ", "-");
+
+		Categoria categoriaExiste = categoriaRepository.findByNome(categoria.getNome());
+
+		if (categoriaExiste != null) {
+
+			redirectAttributes.addFlashAttribute("message", "A Categoria existe, escolha outra");
 			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
-			redirectAttributes.addFlashAttribute("categoria", categoria);
-		}
-		else {
+		} else {
 			categoria.setSlug(slug);
-		
+
 			categoriaRepository.save(categoria);
 		}
-		
+
 		return "redirect:/admin/categorias/actualizar/" + categoria.getId();
 	}
-	
+
 	@GetMapping("/remover/{id}")
 	public String remover(@PathVariable int id, RedirectAttributes redirectAttributes) {
-		
+
 		categoriaRepository.deleteById(id);
-		
+
 		redirectAttributes.addFlashAttribute("message", "Categoria removida com Sucesso");
 		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
-				
+
 		return "redirect:/admin/categorias";
 	}
-	
+
 	@PostMapping("/reorder")
 	public @ResponseBody String reorder(@RequestParam("id[]") int[] id) {
-		
+
 		int count = 1;
 		Categoria categoria;
-		
+
 		for (int idCategoria : id) {
 			categoria = categoriaRepository.getOne(idCategoria);
 			categoria.setSorting(count);
 			categoriaRepository.save(categoria);
 			count++;
-			
+
 		}
 		return "ok";
 	}
-	
-	
 
 }
